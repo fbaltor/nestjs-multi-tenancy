816a0e051864c2c72db7e3ce6b90a7010176c1f2
Felipe Baltor <fbaltor@gmail.com>
2021-03-16 16:26:27 -0300

    minor: adjustments in start.sh

--- start.sh
+++ start.sh
@@ -9,11 +9,11 @@ fi
 sudo chmod +x ./auth/*.sh
 
 if [ ! -d "./auth/db/dbdata" ]; then
-  docker-compose -f ./backend/docker-compose.yml up -d
   docker-compose -f ./auth/docker-compose.yml up -d
+  docker-compose -f ./backend/docker-compose.yml up
   ./auth/import-realm.sh
 else
-  docker-compose -f ./backend/docker-compose.yml up -d
   docker-compose -f ./auth/docker-compose.yml up -d
+  docker-compose -f ./backend/docker-compose.yml up
 fi
 
