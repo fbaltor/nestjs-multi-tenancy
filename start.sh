4a6ac7f1a1230cb70d7c7e8dec88abe7f651d0b4
Felipe Baltor <fbaltor@gmail.com>
2021-03-16 14:34:27 -0300

    improve: start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+
 cd backend/api
 npm install
 cd ../..
-docker-compose -f ./backend/docker-compose.yml up -d
-docker-compose -f ./auth/docker-compose.yml up -d
+
+sudo chmod +x ./auth/{*.sh}
+
+if [ ! -d "./auth/db/dbdata" ]; then
+  docker-compose -f ./backend/docker-compose.yml up -d
+  docker-compose -f ./auth/docker-compose.yml up -d
+  ./auth/import-realm.sh
+else
+  docker-compose -f ./backend/docker-compose.yml up -d
+  docker-compose -f ./auth/docker-compose.yml up -d
+fi
+
