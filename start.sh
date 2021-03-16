7b4ae257ddf26c4f91425afa63d037b2f8692e91
Felipe Baltor <fbaltor@gmail.com>
2021-03-16 15:11:42 -0300

    fix: fixed start.sh chmod in .auth/*.sh command

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cd backend/api
-npm install
-cd ../..
+if [ ! -d "./backend/api/node_modules" ]; then
+  cd backend/api
+  npm install
+  cd ../..
+fi
 
-sudo chmod +x ./auth/{*.sh}
+sudo chmod +x ./auth/*.sh
 
 if [ ! -d "./auth/db/dbdata" ]; then
   docker-compose -f ./backend/docker-compose.yml up -d
