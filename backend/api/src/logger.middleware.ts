e55f02616d1b1158d726e7fe0288bbc12cb73a6f
Felipe Baltor <fbaltor@gmail.com>
2021-03-17 12:32:57 -0300

    minor: logger.middleware.ts

--- backend/api/src/logger.middleware.ts
+++ backend/api/src/logger.middleware.ts
@@ -2,9 +2,9 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 function parseJwt(token) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
     Buffer.from(base64, 'base64')
       .toString()
       .split('')
